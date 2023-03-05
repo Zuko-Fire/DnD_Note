5c03bc2dafde70218035e6acd92669559ac0922a
Zuko <dani4200256@gmail.com>
2023-03-05 16:01:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dndnote/Main.kt
@@ -0,0 +1,31 @@
+package com.example.dndnote
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Main : Fragment() {
+
+    companion object {
+        fun newInstance() = Main()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
